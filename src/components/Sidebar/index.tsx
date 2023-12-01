68776f36b376acec47f465a36638f3e956162f7f
Pablo Martins <pablomartins1515@hotmail.com>
2023-11-30 23:01:49 -0300

    Add files via upload

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,51 @@
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { Logo } from './Logo'
+import { NavItem } from './NavItem'
+import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+
+export function Sidebar() {
+  return (
+    <aside className=" flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 Otext-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
+
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+      </div>
+
+      <UsedSpaceWidget />
+
+      <Profile />
+    </aside>
+  )
+}
